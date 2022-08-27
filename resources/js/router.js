bd7b1199531f3d5a6a7d680c76c73ac08fef2735
korvlad21 <vladek2000@inbox.ru>
2022-08-27 21:54:49 +0300

    0 урок

---
+++ resources/js/router.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+export default new VueRouter({
+    mode:'history',
+    routes: [
+        {
+            path: 'fruits', component: () => import('./components/fruit/Index'),
+            name: 'fruit.index'
+        }
+    ]
+})
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>JWT SPA</title>
+</head>
+<body class="p-5">
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+</html>
