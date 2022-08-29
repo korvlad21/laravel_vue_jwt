71579d9c947db4297de05059ef28080c8762c82e
Maximilyanus29 <vladek2000@inbox.ru>
2022-08-29 14:45:03 +0300

    2 урок

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,15 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+
+    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function() {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Route::group(['namespace' => 'User', 'prefix' => 'users'], function() {
     Route::post('/', 'StoreController');
 });
 
-Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function() {
-    Route::get('/', 'IndexController');
-});
+
 
